bad580b1fe81216f203b0dfde94bc1c04760b44b
Oceanicrasin <107171751+Oceanicrasin@users.noreply.github.com>
2022-07-14 13:12:04 +0100

    Create Change_calculator.py

---
+++ Repl code/Coding club/Objective 11/Change_calculator.py
@@ -0,0 +1,74 @@
+#  Change Calculator
+# while True:
+#   amount=float(input("Enter how much change you need: "))
+#   fiftyPound=0
+#   twentyPound=0
+#   tenPound=0
+#   fivePound=0
+#   twoPound=0
+#   onePound=0
+#   fiftyPence=0
+#   twentyPence=0
+#   tenPence=0
+#   fivePence=0
+#   twoPence=0
+#   onePence=0
+#   while amount>=50:
+#     amount=amount-50
+#     fiftyPound=fiftyPound+1
+
+#   while amount>=20:
+#     amount = amount - 20
+#     twentyPound=twentyPound+1
+
+#   while amount >=10:
+#     amount=amount-10
+#     tenPound=tenPound+1
+
+#   while amount>=5:
+#     amount=amount-5
+#     fivePound=fivePound+1
+    
+#   while amount>=2:
+#     amount=amount-2
+#     twoPound=twoPound+1
+    
+#   while amount>=1:
+#     amount=amount-1
+#     onePound=onePound+1
+  
+#   while amount>=0.50:
+#     amount=amount-0.50
+#     fiftyPence=fiftyPence+1
+  
+#   while amount>=0.20:
+#     amount=amount-0.20
+#     twentyPence=twentyPence+1
+  
+#   while amount>=0.10:
+#     amount=amount-0.10
+#     tenPence=tenPence+1
+  
+#   while amount>=0.05:
+#     amount=amount-0.05
+#     fivePence=fivePence+1
+  
+#   while amount>=0.02:
+#     amount=amount-0.02
+#     twoPence=twoPence+1
+    
+#   while amount>0.009:
+#     amount=amount-0.01
+#     onePence=onePence+1
+#   print("number of £50:",fiftyPound)
+#   print("number of £20:",twentyPound)
+#   print("Number of £10:",tenPound)
+#   print("number of £5: ",fivePound)
+#   print("number of £2: ",twoPound)
+#   print("number of £1: ",onePound)
+#   print("number of 50p:",fiftyPence)
+#   print("number of 20p:",twentyPence)
+#   print("number of 10p:",tenPence)
+#   print("number of 5p: ",fivePence)
+#   print("number of 2p: ",twoPence)
+#   print("number of 1p: ",onePence)
